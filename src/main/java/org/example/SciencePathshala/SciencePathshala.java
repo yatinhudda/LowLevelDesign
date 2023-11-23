5c2645913f1957873797f8647323cf9ea66bf48b
yatinhudda <yatinchaudhary72@gmail.com>
2023-11-23 23:10:21 +0530

    Developed a Main class

--- src/main/java/org/example/SciencePathshala/SciencePathshala.java
+++ src/main/java/org/example/SciencePathshala/SciencePathshala.java
@@ -1,5 +1,15 @@
 package org.example.SciencePathshala;
 
 public class SciencePathshala {
-
+    public static void main(String[] args){
+        String userName = "yatin123@yahoo.com", userPassword = "hudda@123";
+        UserCredentialsValidator userCredentialsValidator =
+                new UserCredentialsValidator(userName, userPassword);
+        CourseModules courseModules = new CourseModules();
+        if(userCredentialsValidator.validateUser()){
+            System.out.println(courseModules.getModules(userName));
+        }else{
+            System.out.println("Invalid User");
+        }
+    }
 }
