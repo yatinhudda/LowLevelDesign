424a7fbe3b4410c007f34722636e15789a786540
yatinhudda <yatinchaudhary72@gmail.com>
2023-11-29 00:24:30 +0530

    Intro to static vaariable

---
+++ src/main/java/org/example/AccessModifiers/LearnStaticKeyword.java
@@ -0,0 +1,27 @@
+package org.example.AccessModifiers;
+
+public class LearnStaticKeyword {
+    /*
+    We will learn here that how can we calculate
+    the number of object created of any class.
+     */
+
+    public static void main(String[] args){
+        Student s1 = new Student("Akshay", "Panwar", 1,
+                987654321, 80);
+
+        Student s2 = new Student("Yatender", "Hudda", 2,
+                987654321, 90);
+
+        Student s3 = new Student("Shivesh", "Sharma", 3,
+                987654321, 99);
+
+        Student s4 = new Student("Nishant", "bansla", 4,
+                987654321, 56);
+
+        System.out.println("Total number of students are : " + Student.numberOfStudents);
+    }
+
+
+
+}
---
+++ src/main/java/org/example/AccessModifiers/Student.java
@@ -0,0 +1,61 @@
+package org.example.AccessModifiers;
+
+public class Student {
+
+    private String firstName;
+    private String lastname;
+    private int studentID;
+    private double mobileNumber;
+    private int studentMarks;
+
+    public static int numberOfStudents = 0;
+
+    public Student(String firstName, String lastname, int studentID, double mobileNumber, int studentMarks) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.studentID = studentID;
+        this.mobileNumber = mobileNumber;
+        this.studentMarks = studentMarks;
+        numberOfStudents++;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setMobileNumber(double mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setStudentMarks(int studentMarks) {
+        this.studentMarks = studentMarks;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public double getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public int getStudentMarks() {
+        return studentMarks;
+    }
+}
